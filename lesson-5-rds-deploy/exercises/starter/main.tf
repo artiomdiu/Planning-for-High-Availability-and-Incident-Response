c38a27bbd380e2e17fa87fc7848c5c425813cc2e
ad <artiomd@gmail.com>
2022-06-15 20:57:43 +0300

    Fixed azs and some more

--- lesson-5-rds-deploy/exercises/starter/main.tf
+++ lesson-5-rds-deploy/exercises/starter/main.tf
@@ -30,7 +30,10 @@ locals {
 
    account_owner = local.name
    name          = "${local.name}-project"
-   azs           = []
+   azs           = ["us-west-1b", "us-west-1c"]
+  providers = {
+    aws = aws.usw1
+  }
    private_subnet_tags = {
      "kubernetes.io/role/internal-elb" = 1
    }
