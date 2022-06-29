30512ed5fdd2db624e7132a068623e6b88cb016e
ad <artiomd@gmail.com>
2022-06-29 13:54:02 +0300

    Commented out ec2_sg part

--- project/starter-code/zone2/modules/ec2/_out.tf
+++ project/starter-code/zone2/modules/ec2/_out.tf
@@ -2,6 +2,6 @@ output "aws_instance" {
    value = aws_instance.ubuntu
  }
 
- output "ec2_sg" {
-     value = aws_security_group.ec2_sg.id
- }
+#  output "ec2_sg" {
+#      value = aws_security_group.ec2_sg.id
+#  }
